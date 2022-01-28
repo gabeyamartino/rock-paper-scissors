a1e6f56a3f988591002c51f812aa76b849e034a5
Gabe Yamartino <gabeyamartino@gmail.com>
2022-01-27 20:19:07 -0500

    Fixed the case-sensitivity issue. All inputs will work now.

--- main.js
+++ main.js
@@ -6,34 +6,36 @@ let possibleMoves = ["Rock", "Paper", "Scissors"]
 
 
 function playRound(playerSelection, computerSelection) {
+
     if (playerSelection === computerSelection) {
         return "It's a tie!"
-    }  else if (playerSelection == "Rock" && computerSelection == "Scissors") {
+    }  else if (playerSelection == "rock" && computerSelection == "scissors") {
         return "You win! Rock beats Scissors!"
-    }  else if (playerSelection == "Paper" && computerSelection == "Rock") {
+    }  else if (playerSelection == "paper" && computerSelection == "rock") {
         return "Got 'em!! Paper beats Rock!"
-    }  else if (playerSelection == "Scissors" && computerSelection == "Paper") {
+    }  else if (playerSelection == "scissors" && computerSelection == "paper") {
         return "Scissors beats Paper, so you win this round!"
-    }  else if (playerSelection == "Rock" && computerSelection == "Paper") {
+    }  else if (playerSelection == "rock" && computerSelection == "paper") {
         return "Damn... Paper beats Rock, you lose!"
-    }  else if (playerSelection == "Paper" && computerSelection == "Scissors") {
+    }  else if (playerSelection == "paper" && computerSelection == "scissors") {
         return "The computer wins this round. Scissors beats paper."
-    }  else if (playerSelection == "Scissors" && computerSelection == "Rock") {
+    }  else if (playerSelection == "scissors" && computerSelection == "rock") {
         return "Rock beats scissors. You lose this round."
-    }
+    }  
 }
 
+  let playerSelection = prompt("Rock, Paper or Scissors?").toLowerCase();
+    console.log(playerSelection);
+  const computerSelection = computerPlay(possibleMoves).toLowerCase();
+    console.log(computerSelection);
+    console.log(playRound(playerSelection, computerSelection));
 
-  function capitalizeFirstLetter(playerSelection) {
-      return playerSelection.charAt(0).toUpperCase() + playerSelection.slice(1);
-  }
+function game(rounds) {
 
-  let playerSelection = prompt("Rock, Paper or Scissors?")
-  console.log(playerSelection);
-  const computerSelection = computerPlay(possibleMoves);
-  console.log(computerSelection);
-  let areEqual = playerSelection.toUpperCase() === computerSelection.toUpperCase();
-  console.log(playRound(playerSelection, computerSelection));
-
-  playRound();
+}
+(playRound(playerSelection, computerSelection);
+(playRound(playerSelection, computerSelection);
+(playRound(playerSelection, computerSelection);
+(playRound(playerSelection, computerSelection);
+(playRound(playerSelection, computerSelection);
   
