a9562ea7ca38f01e50827835073f8eb5b2305d22
Gabe Yamartino <gabeyamartino@gmail.com>
2022-02-15 18:35:17 -0500

    The game is now working, beginning to create css styling

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ function computerPlay(){
     return possibleMoves[Math.floor(Math.random() * possibleMoves.length)]
 }
 
-let result;
+
 
 function playRound(playerSelection, computerSelection) {
     if (playerSelection === computerSelection) {
@@ -30,7 +30,8 @@ function playRound(playerSelection, computerSelection) {
         return result
     }
 
-playerSelection = ("").toLowerCase();
+playerSelection
+console.log(playerSelection);
 computerSelection = computerPlay().toLowerCase();
 }
 
@@ -46,7 +47,7 @@ function game() {
         computerSelection = computerPlay().toLowerCase();
     }
     for (let i = 1; i <= 5; i++) {
-        callPromptFunction();
+
         callRandomComputer();
         playRound(playerSelection, computerSelection);
         if (result==="player") {
@@ -64,4 +65,107 @@ function game() {
     } else alert ("It's a tie! Refresh to try again.")
 }
 
-game();
+let container = document.querySelector('.threeButtons');
+let buttons = document.querySelectorAll('button');
+let button = document.querySelectorAll('button');
+
+playerSelection = button.id;
+computerSelection = computerPlay().toLowerCase();
+playerPoints = 0;
+computerPoints = 0;
+let result;
+
+const outcome = document.querySelector('.score');
+outcome.style.background = ('pink');
+outcome.style.borderStyle = ('solid');
+outcome.style.borderWidth = ('2px');
+outcome.style.borderColor = ('black');
+
+const results = document.querySelector('.result');
+results.style.background = ('blue');
+results.style.borderStyle = ('solid');
+results.style.borderWidth = ('2px');
+
+document.body.appendChild(outcome);
+
+const endgame = document.querySelector('.end');
+endgame.style.borderStyle = ('solid');
+endgame.style.borderWidth = ('2px');
+document.body.appendChild(endgame);
+
+buttons.forEach((button) => {
+    button.addEventListener('click', () => {
+        playerSelection = button.id;
+        computerSelection = computerPlay().toLowerCase();
+        console.log(playerSelection);
+        console.log(computerSelection);
+
+
+            if (playerSelection === "rock" && computerSelection === "scissors") {
+            results.textContent = ("You win! Rock beats Scissors!");
+            result = "player";
+        }  else if (playerSelection === "paper" && computerSelection === "rock") {
+            results.textContent = ("Got 'em!! Paper beats Rock!");
+            result = "player";
+        }  else if (playerSelection === "scissors" && computerSelection === "paper") {
+            results.textContent = ("Scissors beats Paper, so you win this round!");
+            result = "player" ;
+        }  else if (playerSelection === "rock" && computerSelection === "paper") {
+            results.textContent = ("Damn... Paper beats Rock, you lose!");
+            result = "computer";
+        }  else if (playerSelection === "paper" && computerSelection === "scissors") {
+            results.textContent = ("The computer wins this round. Scissors beats paper.");
+            result = "computer";
+        }  else if (playerSelection === "scissors" && computerSelection === "rock") {
+            results.textContent = ("Rock beats scissors. You lose this round.");
+            result = "computer";
+        }  else if (playerSelection === computerSelection) {
+            results.textContent = ("It's a tie!");
+            result = "tie";
+            return result;
+        }
+            
+
+            console.log(result)
+            if (result==="player") {
+                playerPoints += 1
+            }  else if (result==="computer") {
+                computerPoints += 1
+            }  else (result==="tie");
+            outcome.textContent = "Player: " + playerPoints + "\nComputer: " + computerPoints;
+
+            if (playerPoints === 5) {
+                endgame.textContent = ("Congratulations! You won the game! Please refresh the page to play again.");
+                document.getElementById("rock").disabled = true;
+                document.getElementById("paper").disabled = true;
+                document.getElementById("scissors").disabled = true;
+
+            }   else if (computerPoints === 5) {
+                endgame.textContent = ("The computer wins this time... Please refresh the page to play again.")
+                document.getElementById("rock").disabled = true;
+                document.getElementById("paper").disabled = true;
+                document.getElementById("scissors").disabled = true;
+
+            };
+    
+            //work on capping the game at 5 points per player
+
+    });
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
